025172b8f358d071a198b34ec0f2c43f4e2e36fa
Dmytro <dkyrychuk@milosolutions.com>
2023-04-25 22:34:30 +0300

    fix greeting

--- src/components/CustomImage/CustomImage.tsx
+++ src/components/CustomImage/CustomImage.tsx
@@ -3,19 +3,20 @@ import { useState } from 'react';
 import Lightbox from 'react-18-image-lightbox';
 import defaultImg from "../../assets/marketing1.png";
 import 'react-18-image-lightbox/style.css';
+import s from './CustomImage.module.css';
 
 export const CustomImage = ({ src = defaultImg }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
+    <div className={s.imageBlock}>
       <img
-        style={{ width: "100%", maxWidth: 300, margin: "20px 0;" }}
         src={src}
         alt="Marketing"
       />
+      <p>Чтобы увеличить картинку туда сюда. Чтобы увеличить картинку туда сюда</p>
       {/* {isOpen && (
         <Lightbox mainSrc={require('../../assets/marketing1.png')} onCloseRequest={() => setIsOpen(false)} />
       )} */}
-    </>
+    </div>
   );
 };
--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,9 +1,9 @@
-import { CustomImage } from '../CustomImage/CustomImage';
-import s from './Greeting.module.css';
+import { CustomImage } from "../CustomImage/CustomImage";
+import s from "./Greeting.module.css";
 
 type Props = {
   handleStart: () => void;
-}
+};
 
 export const Greeting = ({ handleStart }: Props) => {
   return (
@@ -11,13 +11,23 @@ export const Greeting = ({ handleStart }: Props) => {
       <h1>Урок 1. Что такое интернет-маркетинг</h1>
       <h2>Привет!</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsam doloremque cupiditate pariatur similique rerum unde praesentium dicta doloribus eius eveniet earum repudiandae nobis, ipsum, quibusdam dolore recusandae sit dolorum?
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsam
+        doloremque cupiditate pariatur similique rerum unde praesentium dicta
+        doloribus eius eveniet
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsam
+        doloremque cupiditate pariatur similique rerum unde praesentium dicta
+        doloribus eius eveniet
       </p>
       <CustomImage />
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsam doloremque cupiditate pariatur similique rerum unde praesentium dicta doloribus eius eveniet earum repudiandae nobis, ipsum, quibusdam dolore recusandae sit dolorum?
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsam
+        doloremque cupiditate pariatur similique rerum unde praesentium dicta
+        doloribus eius eveniet earum repudiandae nobis, ipsum, quibusdam dolore
+        recusandae sit dolorum?
       </p>
       <button onClick={handleStart}>Отправиться в путешествие!</button>
     </section>
-  )
-}
+  );
+};
